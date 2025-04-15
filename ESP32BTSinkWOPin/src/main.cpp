7c985c6044cf061773f158d3ce0ee320e1fb8eeb
ggodkin <grgodkin@yahoo.com>
2025-04-15 08:55:03 -0600

    Added pin mapping to MAX98357

--- ESP32BTSinkWOPin/src/main.cpp
+++ ESP32BTSinkWOPin/src/main.cpp
@@ -6,9 +6,9 @@ BluetoothA2DPSink a2dp_sink(i2s);
 
 void setup() {
     auto cfg = i2s.defaultConfig();
-    cfg.pin_bck = 14;
-    cfg.pin_ws = 15;
-    cfg.pin_data = 22;
+    cfg.pin_bck = 14;   // MAX98357 BCLK pin
+    cfg.pin_ws = 15;    // MAX98357 LRC pin
+    cfg.pin_data = 22;  // MAX98357 DIN pin 
     i2s.begin(cfg);
 
     a2dp_sink.start("ESP32BT-I2S");
