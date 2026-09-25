42f48d6958ae8df3e03591877de726ea7c3b001a
Greg Godkin <grgodkin@yahoo.com>
2026-09-25 12:46:26 -0600

    Use MAX98357A-compatible I2S clock rate

--- SAMD21E18-I2S-Synth/include/audio_config.h
+++ SAMD21E18-I2S-Synth/include/audio_config.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define AUDIO_SAMPLE_RATE_HZ   50000u
+#define AUDIO_SAMPLE_RATE_HZ   48000u
 #define AUDIO_TONE_HZ           1000u
 #define AUDIO_SLOT_BITS           32u
 #define AUDIO_CHANNELS              2u
@@ -14,5 +14,18 @@
 #define AUDIO_PIN_MCK0             9u
 
 #define AUDIO_I2S_PIN_MUX           6u
+
+/*
+ * With the SAMD21 DFLL at 48 MHz, the nearest integer I2S clock
+ * division for 48 kHz / 32-bit stereo is 31:
+ *
+ *   48 MHz / 31 / 32 = 48,387.096 Hz
+ *
+ * The MAX98357A accepts this LRCLK frequency (its 30.4-50.4 kHz
+ * range includes it). BCLK is 3.096774 MHz, which is 64x LRCLK.
+ */
+#define AUDIO_I2S_DIVISION          31u
+#define AUDIO_ACTUAL_SAMPLE_RATE_HZ_NUM 48000000u
+#define AUDIO_ACTUAL_SAMPLE_RATE_HZ_DEN (AUDIO_I2S_DIVISION * AUDIO_SLOT_BITS)
 
 #endif
